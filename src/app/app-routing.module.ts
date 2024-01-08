9c20c893d0776c04cf4b00dff2e772eb1fb1022c
albertmacbar <52215754+albertmacbar@users.noreply.github.com>
2024-01-08 00:07:19 -0600

    Fin sección 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
+//El RouteModule le indica a Angular si son rutas principales o rutas hijas
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-
-const routes: Routes = [];
+//Aquí se especifican las rutas de la pagina
+const routes: Routes = [
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'documentation', component: DocumentationComponent },
+  { path: 'contact', component: ContactComponent },
+  //Si la ruta no coincide con ninguna de las anteriores, nos dirigirá a '' que arriba menciona que mostrará el PortafolioComponent
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //Sirve para declarar las rutas hijas
+  // imports: [RouterModule.forChild(routes)],
+
+  //Sirve para declarar las rutas principales de la raís de la aplicación
+  //El 'useHash: true' sirve para que los navegadores web sepan que lo que viene después no es un directorio, sino una parte de la ruta del index.html
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+
+  //Se exporta el 'RouterModule' para que pueda ser utilizado fuera de este módulo
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+//Se exporta el AppRoutingModule porque se neceita usar desde otra clase 
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslocoRootModule } from './transloco-root.module';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
+    BlogComponent,
+    DocumentationComponent,
+    ContactComponent
   ],
+  //Los módulos van en los 'imports'
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,4 +40,6 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+
+}
